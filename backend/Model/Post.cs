0dbc91d41ae94de3c2c5d1400d0e8a7e414ddd9b
victor <victoratnz@outlook.com>
2024-07-07 20:06:51 +1200

    add Post model

---
+++ backend/Model/Post.cs
@@ -0,0 +1,12 @@
+﻿namespace backend.Model;
+
+public class Post
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Content { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public string AuthorName { get; set; } = string.Empty;
+    public Guid AuthorId { get; set; }
+}
