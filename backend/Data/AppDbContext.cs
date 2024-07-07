3197764e6524c882cbe8a1f9421ddf718d59b43f
victor <victoratnz@outlook.com>
2024-07-07 20:39:15 +1200

    Create Database Context

---
+++ backend/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿namespace backend.Data;
+
+using backend.Model;
+using Microsoft.EntityFrameworkCore;
+
+public class AppDbContext:DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Post> Posts { get; set; } = null!;
+}
