d68f5aa570afd2d29c1efb8814e87b18b2d7fc84
victor <victoratnz@outlook.com>
2024-07-09 17:36:24 +1200

    Add PostContent page with detailed post view

--- frontend/src/pages/PostContent.tsx
+++ frontend/src/pages/PostContent.tsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import { observer } from 'mobx-react-lite'
 import postStore from '../store/postStore';
+import { Box, Container, Typography } from '@mui/material';
 
 const PostContent = () => {
-  return (
-    <div>{postStore.selectedPost?.title}</div>
-  )
+    return (
+        <Container>
+            <Box sx={{ marginTop: 5, padding: 5 }}>
+                <Typography variant="h4">{postStore.selectedPost?.title}</Typography>
+                <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                    <Typography variant="h6">Date: {postStore.selectedPost?.createdAt}</Typography>
+                </Box>
+
+                <Box sx={{ marginTop: 5 }}>
+                    <Typography variant="body1">{postStore.selectedPost?.content}</Typography>
+                </Box>
+            </Box>
+        </Container>
+    )
 }
 
 export default observer(PostContent);
