dd95a8f879725bdd4b5a33b69b89c9975fcfa2fa
victor <victoratnz@outlook.com>
2024-07-09 23:03:37 +1200

    Add AccountController for user registration and login

---
+++ backend/Controllers/AccountController.cs
@@ -0,0 +1,99 @@
+﻿using backend.Authentication;
+using backend.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace backend.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AccountController : ControllerBase
+{
+    private readonly UserManager<AppUser> _userManager;
+    private readonly IConfiguration _configuration;
+
+    public AccountController(UserManager<AppUser> userManager, IConfiguration configuration)
+    {
+        _userManager = userManager;
+        _configuration = configuration;
+    }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register([FromBody] RegisterUserModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            var existedUser = await _userManager.FindByNameAsync(model.UserName);
+            if (existedUser != null)
+            {
+                ModelState.AddModelError("UserName", "User name is already taken");
+                return BadRequest(ModelState);
+            }
+
+            var user = new AppUser
+            {
+                UserName = model.UserName,
+                Email = model.Email
+            };
+
+            var result = await _userManager.CreateAsync(user, model.Password);
+
+            if (result.Succeeded)
+            {
+                var token = GenerateToken(model.UserName);
+                return Ok(new { token, user.Id, user.UserName, user.Email });
+            }
+        }
+
+
+        return BadRequest(ModelState);
+    }
+    [HttpPost("login")]
+    public async Task<IActionResult> Login([FromBody] LoginUserModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            var user = await _userManager.FindByNameAsync(model.UserName);
+            if (user != null && await _userManager.CheckPasswordAsync(user, model.Password))
+            {
+                var token = GenerateToken(model.UserName);
+                return Ok(new { token, user.Id, user.UserName, user.Email});
+            }
+        }
+        return BadRequest(new { error = "Invalid user name or password" });
+    }
+
+    private string? GenerateToken(string userName)
+    {
+        var secret = _configuration["JwtConfig:Secret"];
+        var issuer = _configuration["JwtConfig:Issuer"];
+        var audience = _configuration["JwtConfig:Audience"];
+        if (secret is null || issuer is null || audience is null)
+        {
+            throw new ApplicationException("Jwt is not set in the configuration");
+        }
+
+
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var key = Encoding.ASCII.GetBytes(secret);
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(new Claim[]
+            {
+                new(ClaimTypes.Name, userName),
+            }),
+            Expires = null,
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature),
+            Issuer = issuer,
+            Audience = audience
+        };
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        return tokenHandler.WriteToken(token);
+    }
+}
---
+++ backend/Model/LoginUserModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace backend.Model;
+
+public class LoginUserModel
+{
+    [Required(ErrorMessage = "User name is required")]
+    public string UserName { get; set; } = string.Empty;
+
+
+    [Required(ErrorMessage = "Password is required")]
+    public string Password { get; set; } = string.Empty;
+}
