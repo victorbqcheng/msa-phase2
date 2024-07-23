4a884dcc32826d0e7abf4987bf83130c9ffb3f5b
victor <victoratnz@outlook.com>
2024-07-23 23:00:06 +1200

    Add About page stories

---
+++ frontend/src/pages/About.stories.tsx
@@ -0,0 +1,16 @@
+import { Meta, StoryObj } from '@storybook/react'
+
+import About from './About';
+
+
+const meta: Meta<typeof About> = {
+    component: About,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof About>;
+
+export const Default: Story = {
+
+};
