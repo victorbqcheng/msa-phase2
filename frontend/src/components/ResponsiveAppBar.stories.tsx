38f43212975ce47b0539187d0a6821c474d7effb
victor <victoratnz@outlook.com>
2024-07-23 22:51:19 +1200

    Add ResponsiveAppBar stories

---
+++ frontend/src/components/ResponsiveAppBar.stories.tsx
@@ -0,0 +1,53 @@
+import { Meta, StoryObj } from '@storybook/react'
+import ResponsiveAppBar from "./ResponsiveAppBar";
+import { fn } from '@storybook/test';
+import { Route, Routes } from 'react-router-dom';
+
+
+const meta: Meta<typeof ResponsiveAppBar> = {
+    component: ResponsiveAppBar,
+    argTypes: {
+        userName: {
+            control: 'text'
+        }
+    },
+    args: {
+        userName: 'John Doe',
+        onClickLogin: fn(),
+        onClickRegister: fn(),
+        onClickUserProfile: fn(),
+        onClickLogout: fn()
+    },
+    decorators: [
+        (Story, { parameters }) => {
+            console.log("parameters:", parameters);
+            return (
+                <>
+                    <Story />
+                    <div style={{ marginTop: '10px' }}>
+                        <Routes>
+                            <Route path='/' element={<div>Home</div>} />
+                            <Route path='/about' element={<div>About</div>} />
+                        </Routes>
+                    </div>
+                </>
+            )
+        }
+    ],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof ResponsiveAppBar>;
+
+export const Default: Story = {
+    args: {
+        userName: ""
+    }
+};
+
+export const LogIn: Story = {
+    args: {
+        userName: "victor"
+    }
+};
