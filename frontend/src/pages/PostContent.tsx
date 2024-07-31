eb4bec57bbacdcd5f067568293c724576e25c27e
victorbqcheng <46928112+victorbqcheng@users.noreply.github.com>
2024-07-31 16:41:40 +1200

    Refactor date formatting in PostListItem, PostContent, and UserProfile (#4)

--- frontend/src/pages/PostContent.tsx
+++ frontend/src/pages/PostContent.tsx
@@ -2,6 +2,7 @@
 import { observer } from 'mobx-react-lite';
 import postStore from '../store/postStore';
 import { Box, Container, Typography } from '@mui/material';
+import { formatDateTime } from '../utils/utils';
 
 const PostContent = () => {
     return (
@@ -9,7 +10,7 @@ const PostContent = () => {
             <Box sx={{ marginTop: 5, padding: 5 }}>
                 <Typography variant="h4">{postStore.selectedPost?.title}</Typography>
                 <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-                    <Typography variant="h6">Date: {postStore.selectedPost?.createdAt}</Typography>
+                    <Typography variant="h6">Date: {formatDateTime(postStore.selectedPost?.createdAt)}</Typography>
                 </Box>
 
                 <Box sx={{ marginTop: 5 }}>
